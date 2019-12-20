770d84f2236715ab9db29ee0835c40ac0bcebe5f
Michael Kirsch <michaelskirsch@gmail.com>
2019-12-20 09:55:05 -0700

    get, delete endpoints for cats and dogs functional with one example cat and one example dog

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-
+const petRouter = require('./pet-router');
 const app = express();
-app.use(cors());
+const Queue = require('./queue');
+const { CLIENT_ORIGIN, PORT } = require('./config');
+app.use(cors({ origin: CLIENT_ORIGIN }));
+
+const catQueue = new Queue();
+const dogQueue = new Queue();
+app.set('catQueue', catQueue);
+app.set('dogQueue', dogQueue);
+
+app.use('/api', petRouter);
 
 // Catch-all 404
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -13,7 +22,7 @@ app.use(function (req, res, next) {
 
 // Catch-all Error handler
 // Add NODE_ENV check to prevent stacktrace leak
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
@@ -21,6 +30,29 @@ app.use(function (err, req, res, next) {
   });
 });
 
-app.listen(8080,()=>{
-  console.log('Serving on 8080');
+app.listen(PORT, () => {
+  console.log(`Serving on ${PORT}`);
+});
+
+catQueue.enqueue({
+  imageURL:
+    'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+  imageDescription:
+    'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+});
+dogQueue.enqueue({
+  imageURL:
+    'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription:
+    'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
 });
